196ecd5d2060db1e5f3ecbf8e389c8ab54e6cdfe
yazid <yazid.abdrasid@gmail.com>
2020-08-30 21:04:31 +0800

    google map

--- src/app/contactus/contactus.page.ts
+++ src/app/contactus/contactus.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.page.html',
@@ -7,9 +14,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusPage implements OnInit {
 
+  officeData: any
+  officeLocation: any
+  selectedOption: any
+
+  map: GoogleMap;
+  
   constructor() { }
 
   ngOnInit() {
+    this.officeData = {
+      offices: [
+        {
+          id: 1,
+          placeHolderName: "Putrajaya"
+        },
+        {
+          id: 2,
+          placeHolderName: "Melaka"
+        }
+      ]
+    }
+
+    this.selectedOption = this.officeData.offices[0];
+    this.loadMap()
   }
 
+  officeLocationSelect() {
+    console.log(this.officeLocation)
+    this.selectedOption = this.officeData.offices[this.officeLocation - 1];
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map', {
+      // camera: {
+      //   target: {
+      //     lat: 43.0741704,
+      //     lng: -89.3809802
+      //   },
+      //   zoom: 18,
+      //   tilt: 30
+      // }
+    });
+    this.goToMyLocation();
+  }
+
+  goToMyLocation() {
+    this.map.clear();
+
+    // Get the location of you
+    this.map.getMyLocation().then((location: MyLocation) => {
+      console.log(JSON.stringify(location, null ,2));
+
+      // Move the map camera to the location with animation
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        duration: 5000
+      });
+
+      //add a marker
+      let marker: Marker = this.map.addMarkerSync({
+        title: '@ionic-native/google-maps plugin!',
+        snippet: 'This plugin is awesome!',
+        position: location.latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+
+      //show the infoWindow
+      marker.showInfoWindow();
+
+      //If clicked it, display the alert
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        // this.showToast('clicked!');
+      });
+
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
+        (data) => {
+            console.log("Click MAP",data);
+        }
+      );
+    })
+    .catch(err => {
+      //this.loading.dismiss();
+      // this.showToast(err.error_message);
+    });
+  }
 }
