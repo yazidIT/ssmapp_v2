f53e6010989457adc2abf46daa8f2c0a30c89e26
yazid <yazid.abdrasid@gmail.com>
2020-09-02 22:53:43 +0800

    fab test

--- src/app/components/ssmfab/ssmfab.component.ts
+++ src/app/components/ssmfab/ssmfab.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { IonFabButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-ssmfab',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SsmfabComponent implements OnInit {
 
+  @ViewChild('mainfabbtn', { static: false}) private mainFabButton: IonFabButton
+  @ViewChild('mainfabbtn', { read: ElementRef, static: true }) private mainFabButtonElement: ElementRef
   constructor() { }
 
   ngOnInit() {}
+
+  fabMainBtnClicked() {
+    // console.log("Fab main button clicked!")
+    // console.log(this.mainFabButton.activated)
+  }
 
 }
