ad1034e5d1c8a2e38811e0a5070155b210c7e64e
Yazid <yazidrasid@yahoo.co,uk>
2022-03-27 23:06:06 +1100

    register-device service test

--- src/app/services/registerdevice.service.spec.ts
+++ src/app/services/registerdevice.service.spec.ts
@@ -1,6 +1,9 @@
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { inject, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http';
+
 import { RegisterDeviceService } from './registerdevice.service';
 import { Device } from '@ionic-native/device/ngx';
 import { HTTP } from '@ionic-native/http/ngx';
@@ -50,4 +53,57 @@ describe('RegisterdeviceService',() => {
     await expectAsync(service.registerDevice())
       .toBeResolved()
   }));
+});
+
+describe('HttpClient testing', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+  /// Tests begin ///
+
+  it('can test HttpClient.post', () => {
+    let headers = {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+
+    // version 2
+    let apiv2url = 'https://m.ssm.com.my/apiv2/index.php/'
+    let postData = { "uuid" : "oiwjiowr-12332", "os" : "linux", "hash" : "dfdsfggwre" }
+    let urlEndpoint = apiv2url + 'device/register'
+    httpClient.post(urlEndpoint, postData, {headers})
+      .subscribe(data => {
+        expect(data).toBeDefined()
+        console.log(data)
+      }
+      );
+  
+    // The following `expectOne()` will match the request's URL.
+    // If no requests or multiple requests matched that URL
+    // `expectOne()` would throw.
+    const req = httpTestingController.expectOne(urlEndpoint);
+  
+    // Assert that the request is a GET.
+    expect(req.request.method).toEqual('POST');
+  
+    // Respond with mock data, causing Observable to resolve.
+    // Subscribe callback asserts that correct data was returned.
+    // req.flush(testData);
+  
+    // Finally, assert that there are no outstanding requests.
+    httpTestingController.verify();
+  });
 });
